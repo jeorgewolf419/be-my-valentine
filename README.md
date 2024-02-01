401cb560915d6e859b9c8239b677dd89fd061c0a
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2024-02-01 12:44:13 -0500

    pushing my code to github

---
+++ README.md
@@ -0,0 +1,22 @@
+# Be my Valantine
+
+A Single Page Website to ask a desired partner to be your valantine
+
+# The Main Functionality
+
+The Main functionality is the No button displayed is unclickable as it runs away from the cursor when close to a set distance.
+
+See the setButtonPosition and distanceFrom Center function in script.js to get a clearer Idea.
+
+When "Yes I iwll be your valantine!" is clicked 500 confetti (a set number) pops on the screen bouncing from top to buttom
+The confetti continues bouncing for 3 secounds (3000 miliseconds) after which an event list is dispalyed on the screen
+
+## Available Dependencies
+
+## Used Technologies
+
+PURE HTML
+CSS
+Vanilla Javascript
+
+## THIS APPLICATION IS DEPLOYED ON GITHUB PAGES
---
+++ script.js
@@ -0,0 +1,82 @@
+const evilButton = document.getElementById('evil-button')
+const OFFSET = 50
+
+evilButton.addEventListener('click', () => {
+  //alert('Nice Try')
+  //window.close()
+})
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.pageX
+  const y = e.pageY
+  const buttonBox = evilButton.getBoundingClientRect()
+  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+  const horizontalOffset = buttonBox.width / 2 + OFFSET
+  const verticalOffset = buttonBox.height / 2 + OFFSET
+  if (Math.abs(horizontalDistanceFrom) <= horizontalOffset && Math.abs(verticalDistanceFrom) <= verticalOffset) {
+    setButtonPosition(
+      buttonBox.x + horizontalOffset / horizontalDistanceFrom * 10,
+      buttonBox.y + verticalOffset / verticalDistanceFrom * 10
+    )
+  }
+})
+
+function setButtonPosition(left, top) {
+  const windowBox = document.body.getBoundingClientRect()
+  const buttonBox = evilButton.getBoundingClientRect()
+
+  if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = windowBox.right - buttonBox.width - OFFSET
+  }
+  if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+    left = windowBox.left + OFFSET
+  }
+  if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+    top = windowBox.bottom - buttonBox.height - OFFSET
+  }
+  if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    top = windowBox.top + OFFSET
+  }
+
+  evilButton.style.left = `${left}px`
+  evilButton.style.top = `${top}px`
+}
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+  return boxPosition - mousePosition + boxSize / 2
+}
+
+function handleYesClick() {
+    document.getElementById('yesButton').classList.add('clicked');
+    startConfetti();
+  }
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function startConfetti() {
+    const confettiContainer = document.getElementById('confettiContainer');
+    let numofConfetti = 500
+    for (let i = 0; i < numofConfetti; i++) {
+      const confetti = document.createElement('div');
+      confetti.classList.add('confetti');
+      confetti.style.left = `${Math.random() * 100}vw`;
+      confetti.style.background = getRandomColor();
+      confetti.style.animationDuration = `${Math.random() * 2 + 1}s`;
+      confettiContainer.appendChild(confetti);
+    }
+
+    setTimeout(() => {
+      // Remove confetti after a delay
+      confettiContainer.innerHTML = '';
+      eventForm.classList.add('show');
+      container.classList.add('noShow');
+    }, 3000); // Adjust the delay (in milliseconds) based on your animation duration
+  }
